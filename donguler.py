f2d5b2b5d3f57143135e040808bcf0732c914c5e
Koraytopuz <91742967+Koraytopuz@users.noreply.github.com>
2023-03-10 21:40:06 +0300

    Add files via upload

---
+++ donguler.py
@@ -0,0 +1,10 @@
+for sayi in range(10,18, 3):
+    print(sayi)
+    for x in"1" range(6):
+        print(x)
+    else:print("Sonunda Bitti")
+    for i in range(10):
+        if i==5:
+            continue
+            print(1)
+
